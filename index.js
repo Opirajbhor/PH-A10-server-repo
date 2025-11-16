3d918d0bbd4d05632e3d2d2c47f36ed994bab798
Opirajbhor <opiraj6@gmail.com>
2025-11-16 23:00:58 +0600

    final server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-// const connectDB = require("./src/Database.js");
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
--- src/models/user.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({});
-
-const User = mongoose.modal("User", userSchema);
-module.exports(User);
